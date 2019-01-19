6bf306681e842f3585b3c42a2b25db80b1b2c853
Ambrose Chua <ambrose@chua.family>
2019-01-19 22:41:50 +0800

    Experimental JavaScript!

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import dgraph from 'dgraph-js-http';
 import anime from 'animejs';
-import AnimeClass from './animeclass.js';
+import AnimeClass from './animeclass';
+import Logic from './logic';
 
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("Pew everything is loaded")
--- client/src/logic.js
+++ client/src/logic.js
@@ -7,6 +7,10 @@ class Logic {
 	#clientStub = new dgraph.DgraphClientStub(ENDPOINT);
 	#dgraphClient = new dgraph.DgraphClient(clientStub);
 
+	async query(query, vars) {
+		const res = await dgraphClient.newTxn().queryWithVars(query, vars);
+		console.log(res);
+	}
+
 }
 
-console.log(window);
